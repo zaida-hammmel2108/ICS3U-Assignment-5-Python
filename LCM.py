d5f2dacb64c5596506fd03d8f2d472486d92c23b
Zaida Hammel <zzaida.hammel@mths.ca>
2022-11-23 23:42:06 +0000

    added deisng

--- LCM.py
+++ LCM.py
@@ -4,26 +4,34 @@
 # Created on: Nov 2022
 # This program determines the lowest common multiple
 
-# code from https://www.javatpoint.com/python-find-lcm
+# code inspired by https://www.javatpoint.com/python-find-lcm
 def main(num1, num2):
-    # selecting the greater number  
-    if num1 > num2:  
-        greater = num1  
+    # selecting the greater number
+    if num1 > num2:
+        greater = num1
 
-    else:  
-        greater = num2  
-    while(True):  
-        if((greater % num1 == 0) and (greater % num2 == 0)):  
-            lcm = greater  
-            break  
-        greater += 1  
-    return lcm    
-  
-# taking input from users  
-user_num1 = int(input("Enter first number: "))  
-user_num2 = int(input("Enter second number: "))  
-# printing the result for the users  
-print("The L.C.M. of", user_num1,"and", user_num2,"is", main(user_num1, user_num2))  
+    else:
+        greater = num2
+    while True:
+        if greater % num1 == 0 and greater % num2 == 0:
+            lcm = greater
+            break
+        greater += 1
+    return lcm
+
+
+# getting input
+user_num1 = int(input("Enter your first integer: "))
+user_num2 = int(input("Enter your second integer: "))
+# printing the result
+print(
+    "The lowest common multiple of",
+    user_num1,
+    "and",
+    user_num2,
+    "is",
+    main(user_num1, user_num2),
+)
 
 
 if __name__ == "__main__":
